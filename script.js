ed403bf27050f289a7240e25968e632c3e95e5ac
Romaniaisnotavailable <45mathbest@protonmail.com>
2026-08-15 09:47:38 +0000

    Update script.js
    Hdbsdfregkj

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
-const sqr = document.querySelector('.sqr');
-document.querySelector('.B1').onclick = () => sqr.style.backgroundColor = 'red';
-document.querySelector('.B2').onclick = () => sqr.style.backgroundColor = 'blue';
-document.querySelector('.B3').onclick = () => sqr.style.backgroundColor = 'green';
-document.querySelector('.B4').onclick = () => sqr.style.backgroundColor = 'yellow';
-document.querySelector('.B5').onclick = () => sqr.style.backgroundColor = 'purple';
-document.querySelector('.B6').onclick = () => sqr.style.backgroundColor = 'black';
-document.querySelector('.B7').onclick = () => sqr.style.backgroundColor = 'brown';
-document.querySelector('.B8').onclick = () => sqr.style.backgroundColor = 'maroon';
-document.querySelector('.B9').onclick = () => sqr.style.backgroundColor = 'black';
+let count = 0;
+let score = 0;
+let timerId = 0;
+
+function startGame() {
+    count = 0;
+    score = 0;
+    gameLoop();
+}
+
+function gameLoop() {
+    const gamezone = document.getElementById("gamezone");
+    
+    gamezone.innerHTML = "";
+    for (let i = 0; i < 8; i++) {
+        const char = document.createElement("div");
+        char.className = "character characterVisible";
+        char.innerHTML = "character " + (i + 1);
+        
+        char.onclick = function() {
+            score -= 2;
+        };
+        
+        gamezone.appendChild(char);
+    }
+
+    const randomNum = Math.floor(Math.random() * 8);
+    const target = gamezone.children[randomNum];
+
+    target.className = "character characterVisible naruto";
+    target.innerHTML = "Лишний";
+
+    target.onclick = function(e) {
+        e.stopPropagation();
+        score++;
+        clearTimeout(timerId);
+        nextTurn();
+    };
+
+    count++;
+    timerId = setTimeout(nextTurn, 500);
+}
+
+function nextTurn() {
+    if (count < 12) {
+        gameLoop();
+    } else {
+        alert("Игра окончена! Ваш счет: " + score);
+    }
+}
